63983645da00344c32b65f1be7f930033d51fc9e
Seva <sevauni@gmail.com>
2022-12-16 00:44:38 +0200

    working anumations

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,57 +1,54 @@
 "use strict";
-
-
-// document.querySelector(".orange-svg-img").getSVGDocument().getElementById("progress-ring__circle").setAttribute("stroke", "black");
-
+let svgImg = null;
 
 window.addEventListener('load', function () {
 
 
 
-  // document.querySelector(".orange-svg-img").getSVGDocument().getElementById("progress-ring__circle").setAttribute("stroke-dashoffset", "1137");
 
-  // document.querySelector(".orange-svg-img").getSVGDocument().getElementById("progress-ring__circle").getAttribute("stroke-dasharray")
-
-let svgImg = document.querySelector(".orange-svg-img").getSVGDocument().getElementById("progress-ring__circle");
-document.querySelector(".orange-svg-img").getSVGDocument().getElementById("progress-ring__circle").setAttribute("stroke-dashoffset", `${getSvgCirclePxAmount(svgImg)}`);
-
-document.querySelector(".orange-svg-div").addEventListener("click", test);
-
+  svgImg = document.querySelector(".orange-svg-img").getSVGDocument().getElementById("progress-ring__circle");
+  
+  setProcentage(svgImg, 100);
+  
+  document.querySelector(".orange-svg-div").addEventListener('click', startTimer,{once: true });
+  //svgImg.setAttribute("stroke-dashoffset", "500");
 
 
-}); 
+});
 
+function startTimer(svgElement = svgImg) {
+  let maxPx = getSvgCirclePxAmount(svgImg);
+  let id = null;
+  let pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 5);
+  function frame() {
+    if (pos === maxPx ) {
+      clearInterval(id);
+      document.querySelector(".orange-svg-div").addEventListener('click', startTimer,{once: true });
+    } else {
+      pos++;
+     setPixels(svgImg, pos);
+    }
+  }
+}
 
-function test(){
+function getSvgCirclePxAmount(svgElement) {
+  let result = Number(svgElement.getAttribute("stroke-dasharray").split(' ')[0]);
+  return result;
+}
 
-console.log("Testing");
-
-
+function setProcentage(svgElement, percents = 100) {
+  let result = scale(percents, 0, 100, getSvgCirclePxAmount(svgElement), 0);
+  svgElement.setAttribute("stroke-dashoffset", `${result}`);
 }
 
 
-// function startTimer(svgElement) {
-//   let id = null;
-//   let pos = 0;
-//   clearInterval(id);
-//   id = setInterval(frame, 5);
-//   function frame() {
-//     if (pos == 100) {
-//       clearInterval(id);
-//     } else {
-//       pos++; 
-//       elem.style.top = pos + "px"; 
-//       elem.style.left = pos + "px"; 
-//     }
-//   }
-// }
-
-
-
- function setProcentConstant(svgElement){
-   return Math.ceil(getSvgCirclePxAmount(svgElement) / 100);
+function setPixels(svgElement, pixels) {
+  let result = getSvgCirclePxAmount(svgElement) - pixels;
+  svgElement.setAttribute("stroke-dashoffset", `${result}`);
 }
 
-function getSvgCirclePxAmount(svgElement){
-  return Number(svgElement.getAttribute("stroke-dasharray").split(' ')[0]);
+function scale(number, inMin, inMax, outMin, outMax) {
+  return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 }
