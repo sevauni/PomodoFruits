7c0acdcbf314de0f0a8b248b459fc8dc79ce8e6a
Seva <sevauni@gmail.com>
2022-12-18 09:52:37 +0200

    release

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,18 +1,20 @@
 "use strict";
+
+//Set consts:
 let svgImg = null;
 let timerTargetTimeMinutes = 1;
 let frameId = null;
-const workTime = 1;
-const relaxTime = 1;
+const workTime = 1;    //Working time duration
+const relaxTime = 1;   //Relax time duration
 const speedCorrectionSync = 57000;
 
 
 const startSound = new Audio('./audio/tick.ogg');
 const readySound = new Audio('./audio/ready.ogg');
 
-const timerStatus = {
-  pause: false,
-  mode: "start"
+const timerStatus = {   
+  pause: false,   //globally set pause timer stops
+  mode: "start"   //4 modes: start,work,relax,pause
 };
 window.addEventListener('load', function () {
   document.querySelector(".flex-container").style.opacity = "100";
@@ -33,15 +35,10 @@ function startTimers() {
       break;
     case "relax":
       startTimerRelax();
-
       break;
     default:
       console.error("Wrong mode slelector");
   }
-
-
-
-
 }
 function startTimerWork() {
   timerTargetTimeMinutes = workTime;
@@ -160,6 +157,9 @@ function setPixels(svgElement, pixels) {
 function scale(number, inMin, inMax, outMin, outMax) {
   return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 }
+
+//SVG image related selectors
+
 function setStartMode() {
   document.querySelector(".orange-svg-img").getSVGDocument().getElementById("g2651").setAttribute("style", "display:none"); //chillbubble
   document.querySelector(".orange-svg-img").getSVGDocument().getElementById("layer4").setAttribute("style", "display:none"); //work setup
@@ -170,8 +170,6 @@ function setStartMode() {
   document.querySelector(".orange-svg-img").getSVGDocument().getElementById("g5644").setAttribute("style", "display:none"); //pause
   document.querySelector(".header-container-under").innerHTML = "Ready!";
   document.querySelector(".header-container-under").style.color = "white";
-
-
 }
 function setWorkMode() {
   document.querySelector(".orange-svg-img").getSVGDocument().getElementById("g2651").setAttribute("style", "display:none"); //chillbubble
@@ -184,10 +182,8 @@ function setWorkMode() {
   document.querySelector(".orange-svg-img").getSVGDocument().getElementById("path1021").setAttribute("style", "display:inline"); //sad eyes
   document.querySelector(".orange-svg-img").getSVGDocument().getElementById("path2816").setAttribute("style", "display:none"); //bubble img
   document.querySelector(".orange-svg-img").getSVGDocument().getElementById("g5644").setAttribute("style", "display:none"); //pause
-
   document.querySelector(".header-container-under").innerHTML = "Working...";
   document.querySelector(".header-container-under").style.color = "#ed8f20ff";
-
 }
 function setRelaxMode() {
   document.querySelector(".orange-svg-img").getSVGDocument().getElementById("g2651").setAttribute("style", "display:inline"); //chillbubble
@@ -207,23 +203,12 @@ function setRelaxMode() {
 function setPauseMode() {
   document.querySelector(".orange-svg-img").getSVGDocument().getElementById("g2651").setAttribute("style", "display:none"); //chillbubble
   document.querySelector(".orange-svg-img").getSVGDocument().getElementById("layer2").setAttribute("style", "display:none"); //bubble
-  //document.querySelector(".orange-svg-img").getSVGDocument().getElementById("layer4").setAttribute("style", "display:inline"); //work setup
   document.querySelector(".orange-svg-img").getSVGDocument().getElementById("layer5").setAttribute("style", "display:none"); //chill
-
-  //document.querySelector(".orange-svg-img").getSVGDocument().getElementById("g1025").setAttribute("style", "display:none"); //hand layer
-  //document.querySelector(".orange-svg-img").getSVGDocument().getElementById("path1704").setAttribute("style", "stroke:#9b6026;stroke-width:2.00561;stroke-opacity:1"); //hand
-
-  //document.querySelector(".orange-svg-img").getSVGDocument().getElementById("path1704").setAttribute("style", ""); //hand
-  //document.querySelector(".orange-svg-img").getSVGDocument().getElementById("path1704").setAttribute("", ""); //hand
-
-  //document.querySelector(".orange-svg-img").getSVGDocument().getElementById("g2672").setAttribute("style", "display:inline"); //laptop
   document.querySelector(".orange-svg-img").getSVGDocument().getElementById("path1696").setAttribute("style", "display:none"); //sad mouth
   document.querySelector(".orange-svg-img").getSVGDocument().getElementById("path1021").setAttribute("style", "display:none"); //sad eyes
   document.querySelector(".orange-svg-img").getSVGDocument().getElementById("g5644").setAttribute("style", "display:inline"); //pause
   document.querySelector(".orange-svg-img").getSVGDocument().getElementById("path2816").setAttribute("style", "display:none"); //bubble img
   document.querySelector(".orange-svg-img").getSVGDocument().getElementById("layer3").setAttribute("style", "display:none"); //normal face
-
   document.querySelector(".header-container-under").innerHTML = "Pause";
   document.querySelector(".header-container-under").style.color = "#ed8f20ff";
-
 }
